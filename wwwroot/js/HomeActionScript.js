c344cefef9eb2fa22ef4262919ef3e15c7ed65bf
andriy makar <mak27arr@gmail.com>
2020-08-09 01:57:38 +0300

    first work version

---
+++ wwwroot/js/HomeActionScript.js
@@ -0,0 +1,22 @@
+﻿function makeStatistic() {
+    var urlFullStr = $("#input_url_data").val();
+    var urlArray = urlFullStr.split("\n");
+    $("#rezalt tbody").empty();
+    var url_base = window.location.origin;
+    urlArray.forEach(function (entry) {
+    $.ajax({
+        url: url_base+'/URLCheker/GetStatistic?urlsString=' + entry,
+        beforeSend: function (xhr) {
+            xhr.overrideMimeType("text/plain; charset=x-user-defined");
+        }
+    })
+        .done(function (data) {
+            var rezalt = $.parseJSON(data);
+            rezalt.forEach(function (rezalt_entry) {
+                console.log(rezalt_entry);
+                $('#rezalt tbody').append('<tr><td>' + rezalt_entry.RequestDate + '</td><td>' + rezalt_entry.Url.UrlName + '</td><td>' + rezalt_entry.ResponseCode + '</td><td>' + rezalt_entry.Title +'</td></tr>');
+            });
+        });
+    });
+    return false;
+}
